d5549ccb91fc5d552cbc6741ff2946ef567109b3
blankletter <90753948+blankletter@users.noreply.github.com>
2022-01-04 17:36:05 +0800

    Add files via upload

---
+++ 作业四.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int isprime(int n)
+{
+	int i;
+ 	for(i=2;i<n;i++)
+   	if(n%i==0)
+	{
+	return 0;
+    }
+	else 
+    {
+ 	return 1;
+    }
+}
+int main()
+{
+	for(int p=2;p<99999;p++)
+	{
+		for(int q=2;q<999999;q++)
+		{
+		if(isprime(q)&&isprime(p)&&isprime(pow(q,p)+pow(p,q))==1)
+		{
+			printf("q=%d p=%d\n",q,p);
+		}
+		}
+	}
+	return 0;
+ } 
