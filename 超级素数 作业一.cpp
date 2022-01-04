b9fcd1305d0874d091454093dc58ccb580bef983
blankletter <90753948+blankletter@users.noreply.github.com>
2022-01-04 19:01:34 +0800

    Add files via upload

---
+++ 超级素数 作业一.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int isprime(int n)
+{
+	int i;
+ 	for(i=2;i*i<=n;i++)
+   	if(n%i==0)
+	return 0;
+ 	return 1;
+}
+int main()
+{	
+	int sum=0;
+	int q=0;
+	for(int i=101;i<10000;i++)
+  	{
+  	if(i>100&&i<1000)
+	{
+		int a,b,c;
+	  	a=i%10;
+	  	b=((i-a)/10)%10;
+	  	c=i%100;
+	  	if(isprime(i)&&isprime(a+b+c)&&isprime(a*a+b*b+c*c))
+	  	{
+	  	printf("%d\n",i);
+		sum=sum+i;
+		q+=1;
+		}
+	}
+	if(i>=1000&&i<10000)
+	{
+		int w,x,y,z;
+		w=i%10;
+		x=((i-w)/10)%10;
+		y=((i-w-x*10)/100)%10;
+		z=i/1000;
+		if(isprime(i)&&isprime(w+x+y+z)&&isprime(w*w+x*x+y*y+z*z))
+		{
+		printf("%d\n",i);
+		sum=sum+i;
+		q+=1;	
+		}	
+	}	
+	}
+	float averg=0;
+	averg=sum/q;
+	printf("\n");
+	printf("%lf\n",averg);
+	return 0;
+}
